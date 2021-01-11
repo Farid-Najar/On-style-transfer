6133694e571c2ea43093a86a242607987df6763a
Farid Najar <farid.najar98@gmail.com>
2021-01-11 14:37:58 +0100

    inputs added

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import train
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    style_target = '/home/farid/Master/M1/S1/Deep_Learning/Project/On-style-transfer/inputs/monet1.jpg'
+    style_target = '/home/farid/Master/M1/S1/Deep_Learning/Project/On-style-transfer/inputs/vangogh1.jpg'
     content_target = '/home/farid/Master/M1/S1/Deep_Learning/Project/On-style-transfer/inputs/photo1.jpg'
     list_loss = train.train(style_target, content_target)
     plt.plot(range(len(list_loss)), list_loss)
